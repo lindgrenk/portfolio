69e40961be415138ea28437ffbe62ccc5f65c052
Kristian Lindgren <Kristian@Kristians-MacBook-Pro.local>
2016-08-20 23:08:52 +0200

    changed structure on folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var browserSync = require('browser-sync').create();
 
 gulp.task('less', function () {
-	return gulp.src('app/less/style.less')
+	return gulp.src('src/less/style.less')
 		.pipe(less())
 		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
+			browsers : ['last 2 versions'],
+			cascade  : false
 		}))
-		.pipe(gulp.dest('app/css'))
+		.pipe(cssmin())
+		.pipe(gulp.dest('dist/css'))
+		.pipe(browserSync.stream())
+});
+
+gulp.task('serve', ['less'], function() {
+    browserSync.init({
+        server: "./",
+        open: false
+    });
+
+    gulp.watch("src/less/*.less", ['less']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
